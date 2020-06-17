cc588f8f570ddd3ba999031591b9aba1d957f5a8
nataliagarciamartin <natalia.garciamartin@spc.ox.ac.uk>
2020-06-17 18:37:04 +0100

    updated theme

--- example_website/hugo_website.R
+++ example_website/hugo_website.R
@@ -1,3 +1,5 @@
 library(blogdown)
-new_site("new_site", theme = "gcushen/hugo-academic", theme_example = TRUE)
+#new_site("new_site", theme = "gcushen/hugo-academic", theme_example = TRUE)
+setwd("/home/nmartin/Desktop/PhD/website/new_site")
+serve_site()
 
